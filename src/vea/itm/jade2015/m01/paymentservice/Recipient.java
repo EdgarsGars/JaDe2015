e0a82deb1de2241a452c7c2f746c77d9676162d9
mkolo <marcisk@venta.lv>
2015-10-31 17:07:03 +0200

    Initial Payment Service structure

---
+++ src/vea/itm/jade2015/m01/paymentservice/Recipient.java
@@ -0,0 +1,15 @@
+package vea.itm.jade2015.m01.paymentservice;
+
+public class Recipient {
+	
+	private String accNr = "LV000000000001";
+
+	private String getAccNr() {
+		return accNr;
+	}
+
+	private void setAccNr(String accNr) {
+		this.accNr = accNr;
+	}
+		
+}
